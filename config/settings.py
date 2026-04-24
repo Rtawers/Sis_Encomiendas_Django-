8adb6090c35bf1d5f19fa27f6c14f00c74bf27e5
Daniel Rodriguez <rtawers2000@gmail.com>
2026-04-23 19:50:50 -0500

    feat: implementar modelos, querysets y admin de la Sesión 03

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool, default=False)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -125,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
